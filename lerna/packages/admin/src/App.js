eca739db3ad68552815e1c3c7dd4102e6120ab4a
刘亚辉 <liuyahui@cvte.com>
2022-05-09 00:14:24 +0800

    Feat: 学习lerna基本使用

---
+++ lerna/packages/admin/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import WaterForm from 'common/components/WaterForm'
+
+function App() {
+  return (
+    <div className="App">
+      <WaterForm />
+    </div>
+  );
+}
+
+export default App;
